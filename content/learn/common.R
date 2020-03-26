0dbd87b847b7ba7fb7158c5a6b0289295b949333
topepo <mxkuhn@gmail.com>
2020-03-26 19:01:48 -0400

    use content/learn/common.R

--- content/learn/common.R
+++ content/learn/common.R
@@ -7,4 +7,31 @@ knitr::opts_chunk$set(
   collapse = TRUE,
   cache.path = "cache/"
 )
-options(width = 80)
+
+options(width = 80, digits = 3)
+
+req_pkgs <- function(x, what = "This article") {
+  x <- sort(x)
+  x <- knitr::combine_words(x, and = " and ")
+  paste0(
+    what,
+    " requires that you have the following packages installed: ",
+    x, "." 
+  )
+}
+
+small_session <- function(pkgs = NULL) {
+  pkgs <- c(pkgs, "recipes", "parsnip", "tune", "workflows", "dials", "dplyr",
+            "broom", "ggplot2", "purrr", "rlang", "rsample", "tibble", "infer",
+            "yardstick", "tidymodels")
+  pkgs <- unique(pkgs)
+  library(sessioninfo)
+  library(dplyr)
+  sinfo <- sessioninfo::session_info()
+  cls <- class(sinfo$packages)
+  sinfo$packages <- 
+    sinfo$packages %>% 
+    dplyr::filter(package %in% pkgs)
+  class(sinfo$packages) <- cls
+  sinfo
+}
