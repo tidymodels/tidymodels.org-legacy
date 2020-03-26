a181302f5ecabb6ef84cb31298613855f8843b14
topepo <mxkuhn@gmail.com>
2020-03-26 18:51:52 -0400

    added two infer articles to intermediate

--- content/learn/common.R
+++ content/learn/common.R
@@ -8,3 +8,28 @@ knitr::opts_chunk$set(
   cache.path = "cache/"
 )
 options(width = 80)
+
+req_pkgs <- function(x, what = "This article") {
+  x <- sort(x)
+  x <- knitr::combine_words(x, and = " and ")
+  paste0(
+    what,
+    " requires that you have the following packages installed: ",
+    x, "." 
+  )
+}
+small_session <- function(pkgs = NULL) {
+  pkgs <- c(pkgs, "recipes", "parsnip", "tune", "workflows", "dials", "dplyr",
+            "broom", "ggplot2", "purrr", "rlang", "rsample", "tibble", 
+            "yardstick", "tidymodels", "infer")
+  pkgs <- unique(pkgs)
+  library(sessioninfo)
+  library(dplyr)
+  sinfo <- sessioninfo::session_info()
+  cls <- class(sinfo$packages)
+  sinfo$packages <- 
+    sinfo$packages %>% 
+    dplyr::filter(package %in% pkgs)
+  class(sinfo$packages) <- cls
+  sinfo
+}
