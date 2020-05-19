6419b92f4b1e862b4e3e81646187fc4b2fbe7a17
topepo <mxkuhn@gmail.com>
2020-05-19 11:53:31 -0400

    better install code

--- static/code/installs.R
+++ static/code/installs.R
@@ -1,10 +1,18 @@
+install.packages("remotes", repos = "https://cran.rstudio.com")
+
 pkg_list <- read.dcf("DESCRIPTION")[,"Imports"]
 pkg_list <- gsub("\n", "", pkg_list, fixed = TRUE)
 pkg_list <- strsplit(pkg_list, ",")[[1]]
 
-install.packages(pkg_list, repos = "https://cran.rstudio.com")
+remotes::install_cran(
+  pkg_list,
+  repos = "https://cran.rstudio.com",
+  upgrade = "alwyas",
+  type = "source",
+  force = TRUE
+)
 
-library(devtools)
+library(remotes)
 
 install_github("rstudio/DT")
 
